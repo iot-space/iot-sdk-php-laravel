e0d9f1783750118d5c5f779b796ab6f30dc052b2
SHIYIKUI <shiyikui@foxmail.com>
2021-08-04 13:37:38 +0800

    .

--- src/Ty/BaseClient.php
+++ src/Ty/BaseClient.php
@@ -3,12 +3,14 @@
 
 namespace IotSpace\Ty;
 
+use Illuminate\Support\Facades\DB;
 use IotSpace\Support\ApiRequest;
 use IotSpace\Exception\IotException;
 use IotSpace\Exception\ErrorCode;
 use IotSpace\Support\HttpMethod;
 use Illuminate\Config\Repository;
 use Illuminate\Support\Facades\Cache;
+use IotSpace\Support\Platform;
 
 abstract class BaseClient
 {
@@ -99,6 +101,16 @@ abstract class BaseClient
             $options['body'] = $body;
         }
         $res = ApiRequest::httpRequest($method, $url, $options);
+        DB::table('iot_log')->insert([
+            'platform'=>Platform::TY,
+            'method'=>$method,
+            'url'=>$url,
+            'res_code'=>$res['code']??'',
+            'res_data'=>var_export($res, true),
+            'post_data'=>var_export($options, true),
+            'message'=>$res['msg']??'',
+            'createtime'=>date('Y-m-d H:i:s'),
+        ]);
         if(!$res['success']){
             throw new IotException($res['msg'], ErrorCode::TY, $res);
         }
--- src/Ys/BaseClient.php
+++ src/Ys/BaseClient.php
@@ -9,6 +9,8 @@ use IotSpace\Exception\ErrorCode;
 use IotSpace\Support\HttpMethod;
 use Illuminate\Config\Repository;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use IotSpace\Support\Platform;
 
 abstract class BaseClient
 {
@@ -77,6 +79,16 @@ abstract class BaseClient
             $options['form_params'] = $postData;
         }
         $res = ApiRequest::httpRequest($method, $url, $options);
+        DB::table('iot_log')->insert([
+            'platform'=>Platform::YS,
+            'method'=>$method,
+            'url'=>$url,
+            'res_code'=>$res['code']??'',
+            'res_data'=>var_export($res, true),
+            'post_data'=>var_export($options, true),
+            'message'=>$res['msg']??'',
+            'createtime'=>date('Y-m-d H:i:s'),
+        ]);
         if((int)$res['code'] !== 200){
             throw new IotException($res['msg'], ErrorCode::YS, $res['data']);
         }
