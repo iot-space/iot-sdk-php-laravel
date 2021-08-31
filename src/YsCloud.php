e86ee4acd7404bdbbaef89664a34f1c6c61abdbb
SHIYIKUI <shiyikui@foxmail.com>
2021-08-31 10:59:54 +0800

    .

--- src/YsCloud.php
+++ src/YsCloud.php
@@ -6,19 +6,27 @@ use IotSpace\Exception\IotException;
 use IotSpace\Ys\BaseClient;
 use IotSpace\Ys\DeviceClient;
 use IotSpace\Ys\DoorClient;
+use IotSpace\Ys\EsDeviceClient;
+use IotSpace\Ys\EsDoorClient;
+use IotSpace\Ys\EsPersonClient;
+use IotSpace\Ys\EsTokenClient;
 use IotSpace\Ys\PersonClient;
 use IotSpace\Ys\SaasClient;
 use IotSpace\Ys\TokenClient;
 
 
 /**
- * 萤石开放平台SDK
+ * 萤石云平台SDK
  *
  * @method static TokenClient TokenClient()
  * @method static PersonClient PersonClient()
  * @method static DoorClient DoorClient()
  * @method static DeviceClient DeviceClient()
  * @method static SaasClient SaasClient()
+ * @method static EsPersonClient EsPersonClient()
+ * @method static EsDoorClient EsDoorClient()
+ * @method static EsDeviceClient EsDeviceClient()
+ * @method static EsTokenClient EsTokenClient()
  *
  * @package IotSpace
  */
